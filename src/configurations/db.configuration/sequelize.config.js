cd7e4374fa3541a2053a9c00e6cdca6ad02dadbd
Fabio-Alfredo <00210121@uca.edu.sv>
2025-07-11 10:36:16 -0600

    :bug: fix: solution bub in connection by database

--- src/configurations/db.configuration/sequelize.config.js
+++ src/configurations/db.configuration/sequelize.config.js
@@ -2,14 +2,14 @@ import { Sequelize } from "sequelize";
 import { currentConfig } from "../common.configuration/config.js";
 
 const sequelize = new Sequelize({
-    database: currentConfig.db.database,
-    username: currentConfig.db.user,
-    password: currentConfig.db.pass,
-    dialect: currentConfig.db.dialect,
+  database: currentConfig.db.name,
+  username: currentConfig.db.user,
+  password: currentConfig.db.pass,
+  dialect: currentConfig.db.dialect,
 
-    dialectOptions:{
-        socketPath: currentConfig.db.host, // For MySQL socket connection
-    }
+  dialectOptions: {
+    socketPath: currentConfig.db.host, // For MySQL socket connection
+  }
 })
 
 export default sequelize;
