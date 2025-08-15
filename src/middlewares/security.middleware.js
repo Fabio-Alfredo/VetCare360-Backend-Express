1cb035cd19d3d569cc8644e8de57d16882898ce0
Fabio-Alfredo <00210121@uca.edu.sv>
2025-08-14 22:40:17 -0600

    :sparkles: feat: add new middleware for validate roles in routes

--- src/middlewares/security.middleware.js
+++ src/middlewares/security.middleware.js
@@ -1,22 +1,44 @@
 import createHttpError from "http-errors";
 import { verify_token } from "../utils/security/jwt.security.js";
 
-export const authMiddleware = async(req, res, next)=>{
-    try{
-        const { authorization} = req.headers;
-        if(!authorization || !authorization.startsWith("Bearer ") || !(authorization.length > 7))
-            return next(createHttpError(401, "No authorization token provided"));
+export const authMiddleware = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    if (
+      !authorization ||
+      !authorization.startsWith("Bearer ") ||
+      !(authorization.length > 7)
+    )
+      return next(createHttpError(401, "No authorization token provided"));
 
-        const token = authorization.substring(7);
-        const decoded = verify_token(token);
+    const token = authorization.substring(7);
+    const decoded = verify_token(token);
 
-        if(!decoded)
-            return next(createHttpError(401, "Invalid authorization token"));
+    if (!decoded)
+      return next(createHttpError(401, "Invalid authorization token"));
 
-        req.user = decoded;
-        req.token = token;
-        next();
-    }catch(e){
-        next(createHttpError(401, e.message || "Unknown error"));
+    req.user = decoded;
+    req.token = token;
+    next();
+  } catch (e) {
+    next(createHttpError(401, e.message || "Unknown error"));
+  }
+};
+
+export const authorizationMiddleware = (requiredRole = []) => {
+  return async (req, res, next) => {
+    if (
+      !req.user ||
+      !req.user.role ||
+      !requiredRole.some((r) => user.roles.includes(r))
+    ) {
+      return next(
+        createHttpError(
+          403,
+          "You do not have permission to access this resource"
+        )
+      );
     }
-}
+    next();
+  };
+};
--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -3,6 +3,7 @@ import { validationResult } from "express-validator";
 export const validateRequest = (req, res, next) => {
   const errors = validationResult(req);
   console.log(errors);
+  
   if (!errors.isEmpty()) {
     const errorMessages = errors.array().map((err) => err.msg);
     return res.status(400).json({
